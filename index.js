2e557193a81e3139e747b17fdb4491a01a90be54
Shubh27129 <shubham27129@gmail.com>
2023-05-20 19:27:41 +0530

    ClassName commit

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ header.style.borderBottom= 'solid 3px #000';
 let addItem=document.getElementById('add-item');
 addItem.style.color="green";
 addItem.style.fontWeight="bold";
+
+//Sharpener GetElementByClassName Task
+let listItem= document.getElementsByClassName('list-group-item');
+listItem[1].style.color="red";
+listItem[3].textContent='Hello';
+
+
+for(let i=0;i<listItem.length;i++)
+{
+    listItem[i].style.backgroundColor="#f4f4f4";
+}
+listItem[2].style.backgroundColor="green";
+
+for(let i=0;i<listItem.length;i++)
+{
+    listItem[i].style.fontWeight='bold';
+}
